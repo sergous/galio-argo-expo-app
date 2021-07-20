d86e414847cc0d072395b4e90d82bbbab63bb55c
Sergei Smirnov <sergous@gmail.com>
2021-07-20 01:42:37 -0400

    Adds Argon components and screens

---
+++ components/argon/screens/index.tsx
@@ -0,0 +1,9 @@
+import Articles from "./Articles";
+import Elements from "./Elements";
+import Home from "./Home";
+import Onboarding from "./Onboarding";
+import Pro from "./Pro";
+import Profile from "./Profile";
+import Register from "./Register";
+
+export { Articles, Elements, Home, Onboarding, Pro, Profile, Register };
---
+++ constants/argon/tabs.ts
@@ -0,0 +1,10 @@
+const tabs = {
+  categories: [
+    { id: "popular", title: "Popular" },
+    { id: "beauty", title: "Beauty" },
+    { id: "fashion", title: "Fashion" },
+    { id: "car_motorcycle", title: "Car & Motorcycle" },
+  ],
+};
+
+export default tabs;
